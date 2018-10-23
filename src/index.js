0fdd4a49e0488aed8f605bee44938a4c83cf46db
Cameron Raymond <15cjkr@queensu.ca>
2018-10-23 19:34:05 -0400

    New branch time

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import 'typeface-roboto'
 import Home from './scenes/Home/Home'
 import registerServiceWorker from './registerServiceWorker';
-
+// heck yeah
 ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
  
