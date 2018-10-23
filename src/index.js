0d3f0f9e5d265fd570c2e3641a5f5bbfae2c8131
Cameron Raymond <15cjkr@queensu.ca>
2018-10-23 19:11:37 -0400

    Test new branch

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import 'typeface-roboto'
 import Home from './scenes/Home/Home'
 import registerServiceWorker from './registerServiceWorker';
-
+ //TEST
 ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
  
