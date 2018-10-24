0634c52597169b8662882d0c82d437cb88f95f0c
Cameron Raymond <15cjkr@queensu.ca>
2018-10-23 20:10:44 -0400

    New branch TEST

---
+++ src/Scenes/Intro/Intro.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Intro.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="background">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import 'typeface-roboto'
-import Home from './scenes/Home/Home'
-import registerServiceWorker from './registerServiceWorker';
-// heck yeah
-ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();
- 
+import Intro from './Scenes/Intro/Intro';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Intro />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
