b14dfd324d10c1ed1b619677b452fff65ba997e0
Cameron Raymond <15cjkr@queensu.ca>
2018-10-25 14:23:03 -0400

    Down arrow transition in

--- src/Scenes/Intro/Intro.js
+++ src/Scenes/Intro/Intro.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Intro.css';
+import Button from '../../Components/button/Button'
+import { Spring } from 'react-spring'
 import Typing from './Typing'
 
 
@@ -8,8 +10,18 @@ class App extends Component {
     return (
       <div className="container" >
         <div className="background">
-        <div className="typePos"><Typing/></div>
+          <div className="typePos"><Typing /></div>
+          <div className="moveButton">
+            <Spring
+              from={{ opacity: 0 }}
+              to={{ opacity: 1 }}
+              tension={120}
+              friction={14}
+              delay={19000}>
+              {props => <div style={props}><Button /></div>}
+            </Spring>
 
+</div>
         </div>
       </div>
     );
