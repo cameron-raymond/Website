d035fc380c11c40e8c206c1baeed44a33ab122e2
Cameron Raymond <15cjkr@queensu.ca>
2018-10-25 14:34:27 -0400

    Have call back working

--- src/Scenes/Portfolio/Portfolio.js
+++ src/Scenes/Portfolio/Portfolio.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './Portfolio.css';
+import { Parallax, ParallaxLayer } from 'react-spring/dist/addons'
 
+
+const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
 
 class App extends Component {
   render() {
     return (
-      <div className="container" >
-        <h1>This my shit</h1>
-      </div>
+       <ParallaxLayer
+            offset={1}
+            speed={0.1}
+            
+            onClick={this.props.onClick.bind(this)}
+            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+            <img src={url('bash')} style={{ width: '40%' }} />
+          </ParallaxLayer>
+       
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -27,23 +27,8 @@ class App extends React.Component {
             <Intro />
           </ParallaxLayer>
 
-          <ParallaxLayer
-            offset={1}
-            speed={0.1}
-            
-            onClick={() => this.parallax.scrollTo(2)}
-            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-
-            <img src={url('bash')} style={{ width: '40%' }} />
-          </ParallaxLayer>
-
-          <ParallaxLayer
-            offset={2}
-            speed={-0}
-            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-            onClick={() => this.parallax.scrollTo(0)}>
-            <img src={url('clients-main')} style={{ width: '40%' }} />
-          </ParallaxLayer>
+          <Portfolio onClick={() => this.parallax.scrollTo(0)}/>
+          
         </Parallax>
       </div>
     )
