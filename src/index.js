b3d35b85b4b92f1903110c5ee18270c96b4d3315
Cameron Raymond <15cjkr@queensu.ca>
2019-01-12 16:40:14 -0500

    added links

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import Header from './Components/header/Header'
 import Intro from './Scenes/Intro/Intro';
 import './index.css'
+import agent_file from './CRaymondResume2019.pdf';
+
 
 
 
@@ -12,10 +14,17 @@ class App extends React.Component {
       <div>
         <div className="header">
           <Header />
+          <div className="info"> 
+            <p>(647) 984-3006</p>
+            <a href="mailto:15cjkr@queensu.ca" style={{ textDecoration: "none" }}><p>15cjkr@queensu.ca</p></a>
+            <a href={agent_file} download="CameronRaymondResume.pdf" style={{ textDecoration: "none" }}>resumé</a>
         </div>
+        </div>
+        
         <div className="footer">
             <Intro />
         </div>
+        
       </div>
     )
   }
