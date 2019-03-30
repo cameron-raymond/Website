d86f761e3cfdc93463cc0a3f21334a0c9feaa2f9
Cameron Raymond <15cjkr@queensu.ca>
2019-03-30 16:39:23 -0400

    Ive spent way too much time on this. TODO: mobile about

--- src/Scenes/About/About.js
+++ src/Scenes/About/About.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-
+import Intro from './IntroAndImg'
+import Overview from '../../Components/CaseComponents/Overview/Overview'
+import Footer from '../../Components/CaseComponents/Footer/Footer'
+import '../../Assets/standardized.css'
+import './about.css'
 
 class About extends Component {
   render() {
+    let books = [<a href="https://www.penguinrandomhouse.ca/books/74189/the-reluctant-fundamentalist-by-mohsin-hamid/9780385663458">the reluctant fundamentalist</a>, <a href="https://www.penguinrandomhouse.ca/books/89308/thinking-fast-and-slow-by-daniel-kahneman/9780385676533">thinking fast and slow</a>,<a href="https://www.amazon.ca/Everybody-Lies-Internet-About-Really/dp/0062390856">everybody lies</a>, <a href="https://www.simonandschuster.com/books/Bowling-Alone/Robert-D-Putnam/9780743203043">bowling alone</a>]
+    let allTimeAlbums = [<a href="https://pitchfork.com/reviews/albums/22035-the-miseducation-of-lauryn-hill/">the miseducation of lauryn hill</a>, <a href="https://pitchfork.com/reviews/albums/2pac-all-eyez-on-me/">all eyez on me</a>, <a href="https://pitchfork.com/reviews/albums/23125-this-old-dog/">this old dog</a>, <a href="https://pitchfork.com/reviews/albums/20390-to-pimp-a-butterfly/">to pimp a butterfly</a>, <a href="https://pitchfork.com/reviews/albums/9371-pet-sounds-40th-anniversary/">pet sounds</a>]
+    let pastYearAlbums = [<a href="https://pitchfork.com/reviews/albums/sons-of-kemet-your-queen-is-a-reptile/">your queen is a reptile</a>, <a href="https://pitchfork.com/reviews/albums/kanye-west-kid-cudi-kids-see-ghosts/">kids see ghosts</a>, <a href="https://pitchfork.com/reviews/albums/parquet-courts-wide-awake/">wide awake!</a>, <a href="https://pitchfork.com/reviews/albums/black-sabbath-paranoid/">paranoid</a>];
     return (
-      <div >
-        <p>TO DO About</p>
+      <div className="caseContainer">
+        <Intro />
+        <Overview headers={["favourite books (past year)", "favourite albums (all time)", "favourite albums (past year)"]}
+          elements={[books,allTimeAlbums,pastYearAlbums]}/>
+              <Footer to={"contact"}linkTitle={"contact"} linkTitleMobile={"contact"}/>
+
       </div>
     );
   }
---
+++ src/Scenes/About/IntroAndImg.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Typist from 'react-typist';
+import Blurb from './RevAbout'
+import portrait from '../../Assets/portrait.JPG'
+import '../../Assets/standardized.css'
+import './about.css'
+
+class Intro extends Component {
+  render() {
+    return (
+      <div className="introAndImg">
+      <div className="introCont">
+        <Typist cursor={{ hideWhenDone: true }} avgTypingDelay={30} className="serif">
+          <span>nice to meet you, my name is <span className="marker">cameron</span></span>
+        </Typist>
+        <Blurb/>
+        </div>
+        <img src={portrait} alt={""} className="aboutImg"/>
+      </div>
+    );
+  }
+}
+
+export default Intro;
