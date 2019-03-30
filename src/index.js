1261918db0cbc9b68585d2955625ab14abfb134b
Cameron Raymond <15cjkr@queensu.ca>
2019-03-30 10:18:13 -0400

    ScrollToTop works

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ReactGA from 'react-ga';
+import ScrollToTop from './Assets/ScrollToTop'
 import Header from './Components/header/Header'
 import Home from'./Scenes/HomePage/Home';
 import RecycleIt from './Scenes/RecycleIt/RecyleIt'
@@ -34,12 +35,14 @@ class Head extends React.PureComponent {
 ReactDOM.render(<Router>
   <div>
     <div style={{position: 'sticky', top: 0, zIndex: 1000}}><Head/></div>
+    <ScrollToTop>
     <Route exact path="/" component={Home} />
     <Route path="/recycleit" component={RecycleIt} />
     <Route path="/DII" component={DII} />
     <Route path="/distDeliveries" component={DistDeliveries} />
     <Route path="/about" component={About}/>
     <Route path="/contact" component={Contact}/>
+    </ScrollToTop>
   </div>
 </Router> 
 , document.getElementById('root'))
