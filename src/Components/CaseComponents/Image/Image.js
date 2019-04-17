f02135e42054fc0a562d68cec33b145312723897
Cameron Raymond <15cjkr@queensu.ca>
2019-04-17 17:27:04 -0400

    Image loading doesn't make everything reveal then push it down

--- src/Components/CaseComponents/Image/Image.js
+++ src/Components/CaseComponents/Image/Image.js
@@ -7,7 +7,7 @@ class Image extends Component {
   render() {
     const isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
     const src = isSafari ? this.props.altSrc : this.props.src
-    return <div className="imgContainer" ><img src={this.src} className="imgStyling" alt={ssrc} /></div>
+    return <div className="imgContainer" ><img src={src} className="imgStyling" alt={src} /></div>
   }
 }
 export default Image;
