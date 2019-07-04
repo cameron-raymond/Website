d0e7491feca3f237072e7376c2c72cd6c4085526
Cameron Raymond <15cjkr@queensu.ca>
2019-07-03 22:10:11 -0400

    updated header css

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiUser, FiMessageSquare, FiClipboard } from 'react-icons/fi';
 import ReactGA from 'react-ga';
-
-import './header.css'
-import '../../Assets/standardized.css'
+import styles from './header.module.css'
 import resume from '../../Assets/CRaymondResume2020.pdf';
 
 class Header extends React.Component {
     render() {
         return (
-            <div className="header">
-                <Link to="/"><p className="textEmphasis onHov">cameron<span className="hideHead"> raymond</span></p></Link>
+            <div className={styles.header}>
+                <Link to="/"><p className="textEmphasis onHov">cameron<span className={styles.hideHead}> raymond</span></p></Link>
                 <div style={{
                     display: "flex",
                     minWidth: 100,
@@ -24,10 +22,10 @@ class Header extends React.Component {
                         eventLabel="clickedResume"
                         to={resume}
                     >
-                    <p className="text primary onHov"><span className="hideIcons"><FiClipboard /></span><span className="hideLinks">resumé</span></p>
+                    <p className="text primary onHov"><span className={styles.hideIcons}><FiClipboard /></span><span className={styles.hideLinks}>resumé</span></p>
                     </ReactGA.OutboundLink>
-                    <Link to="/about"><p className="text onHov" ><span className="hideIcons"><FiUser /></span><span className="hideLinks">about</span></p></Link>
-                    <Link to="/contact"> <p className="text onHov" ><span className="hideIcons"><FiMessageSquare /></span><span className="hideLinks">contact</span></p></Link>
+                    <Link to="/about"><p className="text onHov" ><span className={styles.hideIcons}><FiUser /></span><span className={styles.hideLinks}>about</span></p></Link>
+                    <Link to="/contact"> <p className="text onHov" ><span className={styles.hideIcons}><FiMessageSquare /></span><span className={styles.hideLinks}>contact</span></p></Link>
                 </div>
 
             </div>
