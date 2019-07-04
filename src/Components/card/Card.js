6456f6c6cd0ea21906bbd314907629779b2b8586
Cameron Raymond <15cjkr@queensu.ca>
2019-07-03 21:50:56 -0400

    Changed card to css module

--- src/Components/card/Card.js
+++ src/Components/card/Card.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import reveal from '../HOC/scrollReveal/Reveal'
-import '../../Assets/standardized.css'
-import './card.css'
+import styles from './card.module.css'
 
 
 class Card extends Component {
@@ -9,9 +8,9 @@ class Card extends Component {
   src = this.isSafari ? this.props.altSrc : this.props.src
   render() {
     return (
-      <div className="card" style={{ backgroundImage: `url(${this.src})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
-        <div className="overlay"/>
-        <div className="cardContent">
+      <div className={styles.card} style={{ backgroundImage: `url(${this.src})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
+        <div className={styles.overlay}/>
+        <div className={styles.cardContent}>
           <p className="title light">{this.props.children}</p>
           <p className="text light">{this.props.subtitle}</p>
         </div>
