1ac108583449224ff489a139f83f7253953b08be
Cameron Raymond <15cjkr@queensu.ca>
2019-07-03 21:54:18 -0400

    Changed footer css to module

--- src/Components/CaseComponents/Footer/Footer.js
+++ src/Components/CaseComponents/Footer/Footer.js
@@ -1,15 +1,15 @@
 import React, { PureComponent } from 'react';
 import { Link } from 'react-router-dom';
 import '../../../Assets/standardized.css'
-import './footer.css'
+import styles from './footer.module.css'
 
 class Footer extends PureComponent {
   
   render() {
     return (
-      <div className="footer">
+      <div className={styles.footer}>
       
-        <div className="footInfo">
+        <div className={styles.footInfo}>
           <p className="textEmphasis">&copy; 2019 cameron raymond</p>
           <a href="mailto:c.raymond@queensu.ca" className="text">c.raymond@queensu.ca</a>
           <a href="https://linkedin.com/in/cameron-raymond/" className="text">linkedIn</a>
@@ -17,8 +17,8 @@ class Footer extends PureComponent {
         </div>
 
         <Link to={this.props.to ? this.props.to : '/'}>
-          <p className="serif viewNext onHov">{this.props.linkTitle ? this.props.linkTitle : "what else ya got" }</p>
-          <p className="serif viewNextMobile onHov">{this.props.linkTitleMobile ? this.props.linkTitleMobile : "next"}</p>
+          <p className={styles.viewNext+" serif onHov"}>{this.props.linkTitle ? this.props.linkTitle : "what else ya got" }</p>
+          <p className={styles.viewNextMobile+" serif onHov"}>{this.props.linkTitleMobile ? this.props.linkTitleMobile : "next"}</p>
         </Link>
       </div>
     )
