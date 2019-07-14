3b25c379944780baab0d874fc4a8e6a5b564a7bd
Cameron Raymond <15cjkr@queensu.ca>
2019-07-14 12:27:31 -0400

    Deployed on 07-14-19

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
+git pull
 echo 'cameronraymond.me' > ./build/CNAME
 sed -i -e 's/lastUpdated = \'\''.*\'\'';/lastUpdated = \'\'$(date +"%m-%d-%y")\'';/g' ./src/Components/CaseComponents/Footer/Footer.js
 git add .
-git commit -m "Deploying on $(date +"%m-%d-%y")"
+git commit -m "Deployed on $(date +"%m-%d-%y")"
 git push
