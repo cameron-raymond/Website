cad3652b2bb30453f34c394bd8bc1c8dd3b28b99
Cameron Raymond <15cjkr@queensu.ca>
2019-07-17 11:45:36 -0400

    Deployed on 07-17-19

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 git pull
-echo "🎉🎊Updating CNAME🎉🎊"
+echo "🎉  🎊 Updating CNAME 🎉  🎊"
 echo 'cameronraymond.me' > ./build/CNAME
-echo "😤👊Updating lastUpdated😤👊"
+echo "😤  👊 Updating lastUpdated 😤  👊"
+rm ./src/Components/Footer/Footer.js-e
 sed -i -e 's/lastUpdated = \'\''.*\'\'';/lastUpdated = \'\'$(date +"%m-%d-%y")\'';/g' ./src/Components/CaseComponents/Footer/Footer.js
-echo "😫👀Pushing😫👀"
+echo "😫  👀 Pushing 😫  👀"
 git add .
 git commit -m "Deployed on $(date +"%m-%d-%y")"
 git push
-echo "🍻🥂Deploying🍻🥂"
+echo "🍻  🥂 Deploying 🍻  🥂"
