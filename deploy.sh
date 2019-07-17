e562aaa8fbb056a95a41175d45e0d2c2478e3b63
Cameron Raymond <15cjkr@queensu.ca>
2019-07-17 11:49:32 -0400

    updated shell script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 git pull
-echo "🎉  🎊 Updating CNAME 🎉  🎊"
+echo "🎉  🎊  Updating CNAME 🎉  🎊"
 echo 'cameronraymond.me' > ./build/CNAME
-echo "😤  👊 Updating lastUpdated 😤  👊"
+echo "😤  👊  Updating lastUpdated 😤  👊"
+sed -i -e 's/lastUpdated = \'\''.*\'\'';/lastUpdated = \'\'$(date +"%m-%d-%y")\'';/g' ./src/Components/Footer/Footer.js
 rm ./src/Components/Footer/Footer.js-e
-sed -i -e 's/lastUpdated = \'\''.*\'\'';/lastUpdated = \'\'$(date +"%m-%d-%y")\'';/g' ./src/Components/CaseComponents/Footer/Footer.js
-echo "😫  👀 Pushing 😫  👀"
+echo "😫  👀  Pushing 😫  👀"
 git add .
 git commit -m "Deployed on $(date +"%m-%d-%y")"
 git push
-echo "🍻  🥂 Deploying 🍻  🥂"
+echo "🍻  🥂  Deploying 🍻  🥂"
