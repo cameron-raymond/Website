7a97e13817e478e4a8c74855b87d02dd87f93e02
Cameron Raymond <15cjkr@queensu.ca>
2019-07-31 13:23:04 -0400

    linted index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react"
+import ReactDOM from "react-dom"
 // import { render } from 'react-snapshot';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import ScrollToTop from './Assets/ScrollToTop'
-import Header from './Components/header/Header'
-import Home from './Scenes/HomePage/Home';
-import RecycleIt from './Scenes/RecycleIt/RecyleIt'
-import DII from './Scenes/DigitalInsights/DigInsights'
-import PS from './Scenes/PublicisSapient/PublicisSapient'
-import DistDelivery from './Scenes/DistDelivery/DistDelivery'
-import About from './Scenes/About/About'
-import Contact from './Scenes/Contact/Contact'
-import './index.css'
+// eslint-disable-next-line no-unused-vars
+import ScrollToTop from "./Assets/ScrollToTop"
+// eslint-disable-next-line no-unused-vars
+import Header from "./Components/header/Header"
+import Home from "./Scenes/HomePage/Home";
+import RecycleIt from "./Scenes/RecycleIt/RecyleIt"
+import DII from "./Scenes/DigitalInsights/DigInsights"
+import PS from "./Scenes/PublicisSapient/PublicisSapient"
+import DistDelivery from "./Scenes/DistDelivery/DistDelivery"
+import About from "./Scenes/About/About"
+import Contact from "./Scenes/Contact/Contact"
+import "./index.css"
 
 class MyApp extends React.PureComponent {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Header />
-          <ScrollToTop>
-            <Route exact path="/" component={Home} />
-            <Route path="/recycleit" component={RecycleIt} />
-            <Route path="/DII" component={DII} /> 
-            <Route path="/distDelivery" component={DistDelivery} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/publicissapient" component={PS}/>
-          </ScrollToTop>
-        </div>
-      </Router>
-    )
-  }
+	render() {
+		return (
+			<Router>
+				<div>
+					<Header/>
+					<ScrollToTop>
+						<Route exact path="/" component={Home} />
+						<Route path="/recycleit" component={RecycleIt} />
+						<Route path="/DII" component={DII} /> 
+						<Route path="/distDelivery" component={DistDelivery} />
+						<Route path="/about" component={About} />
+						<Route path="/contact" component={Contact} />
+						<Route path="/publicissapient" component={PS}/>
+					</ScrollToTop>
+				</div>
+			</Router>
+		)
+	}
 }
 
 
-ReactDOM.render(<MyApp/>, document.getElementById('root'))
+ReactDOM.render(<MyApp/>, document.getElementById("root"))
