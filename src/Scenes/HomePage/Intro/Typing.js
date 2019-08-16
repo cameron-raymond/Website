54bc6dbaa390439197964de8b347e78e1d1ede23
Cameron Raymond <15cjkr@queensu.ca>
2019-08-15 20:23:25 -0400

    Deployed on 08-15-19

--- src/Scenes/HomePage/Intro/Typing.js
+++ src/Scenes/HomePage/Intro/Typing.js
@@ -23,7 +23,7 @@ class Typing extends PureComponent {
 				<span>at Queen&apos;s University </span>
 				<Typist.Delay ms={800} />
 				<br/>
-				<span className="text">curently applying to <span className="italic">graduate school</span> and looking for <span className="italic">summer 2020</span> opprotunities</span>
+				<span className="text">curently applying to <span className="italic">graduate school</span> and looking for <span className="italic">summer 2020</span> opportunities</span>
 
 
 			</Typist>
