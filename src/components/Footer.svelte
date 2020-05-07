71586815fc1819cd6c5dca93d4c914bd8e06fc9d
Cameron Raymond <15cjkr@queensu.ca>
2020-05-07 15:50:52 -0400

    Added medium to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,14 +1,15 @@
 <script>
   import Emoji from "./Emoji.svelte";
-  import { FaLinkedinIn, FaGithub } from "svelte-icons/fa";
+  import { FaLinkedinIn, FaGithub, FaMediumM } from "svelte-icons/fa";
   import { getOutboundLink } from "../utils/getOutboundLink.js";
   import IoIosMail from "svelte-icons/io/IoIosMail.svelte";
   let year = "2020";
   let month = "May";
   let day = "07";
-  let linkedIn = "https://www.linkedin.com/in/cameron-raymond/"
-  let github = "https://github.com/cameron-raymond"
-  let email = "mailto:cameronraymond534@gmail.com"
+  let linkedIn = "https://www.linkedin.com/in/cameron-raymond/";
+  let github = "https://github.com/cameron-raymond";
+  let email = "mailto:cameronraymond534@gmail.com";
+  let medium = "https://medium.com/cameron-raymond";
 </script>
 
 <style>
@@ -29,7 +30,7 @@
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    width: 8em;
+    width: 10em;
   }
   .divider {
     height: 1.5rem;
@@ -47,7 +48,7 @@
       height: 1rem;
     }
     .links {
-      width: 6.5em;
+      width: 8em;
     }
   }
 </style>
@@ -60,22 +61,45 @@
     by me.
     <br />
     Last updated
-    <a aria-label="{month} {day}, {year}" href="https://www.onthisday.com/events/{month}/{day}">
+    <a
+      aria-label="{month}
+      {day}, {year}"
+      href="https://www.onthisday.com/events/{month}/{day}">
       {month} {day}, {year}
     </a>
     .
   </p>
   <div class="links">
-    <a aria-label="Email" href={email} class="icon" on:click={() => getOutboundLink(email)}>
+    <a
+      aria-label="Email"
+      href={email}
+      class="icon"
+      on:click={() => getOutboundLink(email)}>
       <IoIosMail />
     </a>
     <div class="divider" />
-    <a aria-label="LinkedIn" href={linkedIn} class="icon" on:click={() => getOutboundLink(linkedIn)}>
+    <a
+      aria-label="LinkedIn"
+      href={linkedIn}
+      class="icon"
+      on:click={() => getOutboundLink(linkedIn)}>
       <FaLinkedinIn />
     </a>
     <div class="divider" />
-    <a aria-label="Github" href={github} class="icon" on:click={() => getOutboundLink(github)}>
+    <a
+      aria-label="Github"
+      href={github}
+      class="icon"
+      on:click={() => getOutboundLink(github)}>
       <FaGithub />
+    </a>
+    <div class="divider" />
+    <a
+      aria-label="Medium"
+      href={medium}
+      class="icon"
+      on:click={() => getOutboundLink(medium)}>
+      <FaMediumM />
     </a>
   </div>
 </div>
