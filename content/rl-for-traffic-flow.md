bcdb8b931f14d3cca4ab2d5a123a3de534d778b8
Cameron Raymond <15cjkr@queensu.ca>
2020-05-08 11:16:08 -0400

    Fixed nav bug

--- content/rl-for-traffic-flow.md
+++ content/rl-for-traffic-flow.md
@@ -8,5 +8,42 @@ link: "<a aria-label='Repo' href='https://github.com/ZaneLittle/Traffic-Light-Si
 date: "Dec. '19"
 ---
 
-Your markdown content.
+* **Cameron Raymond** - *Computer Science, Queen's University*
+* **Hugh Corley** - *Applied Mathematics, Queen's University*
+* **Leonard Zhao** - *Biomedical Computing, Queen's University*
+* **Nicolas Wlodek** - *Cognitive Computing, Queen's University*
+* **Ross Hill** - *Software Design, Queen's University*
+* **Zane Little** - *Cognitive Computing, Queen's University*
 
+** **
+
+## Q-Learning for Traffic Signal Control
+
+![Gif broke!](https://media.giphy.com/media/ZDF8gHKyt5nQzwtwSU/giphy.gif)
+
+### Performance Over Time
+
+#### Loop Route
+
+![Loop Softmax Over Time](https://raw.githubusercontent.com/ZaneLittle/Traffic-Light-Simulation/master/images/DailyAverage-LoopRoute-Softmax.png)
+
+#### Normal Route
+
+![Normal Route Over Time](https://raw.githubusercontent.com/ZaneLittle/Traffic-Light-Simulation/master/images/DailyAvg_NormalRoute_%20EGreedy.png)
+
+## Dependencies
+
+* `pip3 install -r requirements.txt`
+
+## Config Values That You Can Customize
+
+1) Set the file that you'd like to save the Q-Table to in config.
+2) Set the Q-Table to load from (if you want to train from scratch you'll have the option to from command line).
+3) Set the number of years and day per years (default is 1 year of 10 days).
+4) Set the policy that you'd like to use (`egreedy` or `softmax`)
+5) Set the environment dynamics in the config with `ENV_CONSTANTS["ROUTE"]` (either `normal`, `loopy` or `simpleLoopy`)
+
+## To Run
+
+* **Train/Plot** -  `python3 Main.py`
+* **Visualize** - `python3 Visualizer.py` (this will also train but much slower due to as it has to render each new state)
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,9 +18,8 @@
   let y;
   export let posts;
 
-  onMount(() => {
-    visible = true;
-  });
+  onMount(() => (visible = true));
+  onDestroy(() => (visible = false));
 </script>
 
 <style>
@@ -103,47 +102,46 @@
 <svelte:window bind:scrollY={y} />
 
 <div class="cont">
-  {#if visible}
-    <div class="intro">
-      <h1
-        in:fly={{ y: 50, duration: 500 }}
-        out:fly={{ delay: 50, y: 50, duration: 500 }}>
-        Hello
-        <Emoji symbol="👋" />
-      </h1>
-      <p
-        in:fly={{ delay: 300, y: 50, duration: 500 }}
-        out:fly={{ y: 50, duration: 500 }}>
-        I'm Cameron - a data scientist
-        <Emoji symbol="📈" />
-        and incoming graduate student
-        <Emoji symbol="🧑‍💻" />
-        at the
-        <a aria-label="University of Oxford" href="http://www.ox.ac.uk/">
-          University of Oxford
-        </a>
-        <Emoji symbol="🏫🏯" />
-      </p>
-      <p
-        in:fly={{ delay: 350, y: 50, duration: 500 }}
-        out:fly={{ y: 50, duration: 500 }}>
-        This is a new version of my portfolio, so be sure to check back for more
-        posts on my work!
-      </p>
-    </div>
-    <span
-      in:fade={{ duration: 500, delay: 3000 }}
-      out:fade={{ duration: 0 }}
-      style="opacity: {1 - Math.max(0, y / 500)}"
-      class="down-arrow">
-      <FaAngleDown />
-    </span>
-  {/if}
+  <!-- {#if visible} -->
+  <div class="intro">
+    <h1
+      in:fly={{ y: 50, duration: 500 }}
+      out:fly={{ delay: 50, y: 50, duration: 500 }}>
+      Hello
+      <Emoji symbol="👋" />
+    </h1>
+    <p
+      in:fly={{ delay: 300, y: 50, duration: 500 }}
+      out:fly={{ y: 50, duration: 500 }}>
+      I'm Cameron - a data scientist
+      <Emoji symbol="📈" />
+      and incoming graduate student
+      <Emoji symbol="🧑‍💻" />
+      at the
+      <a aria-label="University of Oxford" href="http://www.ox.ac.uk/">
+        University of Oxford
+      </a>
+      <Emoji symbol="🏫🏯" />
+    </p>
+    <p
+      in:fly={{ delay: 350, y: 50, duration: 500 }}
+      out:fly={{ y: 50, duration: 500 }}>
+      This is a new version of my portfolio, so be sure to check back for more
+      posts on my work!
+    </p>
+  </div>
+  <span
+    in:fade={{ duration: 500, delay: 3000 }}
+    out:fade={{ duration: 0 }}
+    style="opacity: {1 - Math.max(0, y / 500)}"
+    class="down-arrow">
+    <FaAngleDown />
+  </span>
+  <!-- {/if} -->
   <img
     src="intro.svg"
     alt=""
     class="intro-svg"
     out:fade={{ duration: 50, delay: 100 }} />
-
 </div>
-<Cards {posts}/>
+<Cards {posts} />
