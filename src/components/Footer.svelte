84ef7aec9aa1ca17aac1f35ccee66e680b1f67af
Cameron Raymond <15cjkr@queensu.ca>
2020-06-08 10:14:15 -0400

    Deployed on 06-08-20

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,7 +5,7 @@
   import IoIosMail from "svelte-icons/io/IoIosMail.svelte";
   let year = "2020";
   let month = "June";
-  let day = "06";
+  let day = "08";
   let linkedIn = "https://www.linkedin.com/in/cameron-raymond/";
   let github = "https://github.com/cameron-raymond/";
   let email = "mailto:cameronraymond534@gmail.com?subject=Let's%20Talk%20Data";
