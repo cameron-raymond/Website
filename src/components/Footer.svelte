3ed692f9b2550c0ab70918bf069ac5f129bcec5a
Cameron Raymond <cameron.raymond@hey.com>
2020-08-24 09:14:44 -0400

    Deployed on 08-24-20

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -9,7 +9,7 @@
   import { getOutboundLink } from "../utils/link.js";
   let year = "2020";
   let month = "August";
-  let day = "16";
+  let day = "24";
   let linkedIn = "https://www.linkedin.com/in/CJKRaymond/";
   let github = "https://github.com/cameron-raymond/";
   let email = "mailto:cameron.raymond@hey.com?subject=Let's%20Talk%20Data";
