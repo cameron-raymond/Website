325a916f71d003869d88c7cbabc5e8739eb4bf17
Cameron Raymond <cameron.raymond@hey.com>
2022-10-12 08:22:38 -0400

    hopefully this helps with booklist 404

--- src/components/HiddenLinks.svelte
+++ src/components/HiddenLinks.svelte
@@ -10,5 +10,6 @@
 <a href="blog/" aria-label=" ">blog</a>
 <a href="sitemap.xml" aria-label=" ">sitemap</a>
 <a href="about/booklist/" aria-label=" ">blog</a>
+<a href="about/booklist.json" aria-label=" ">blog</a>
 <a href="content/political-community-embedding/Measuring_Alignment_of_Online_Grassroots_Political_Communities_with_Political_Campaigns.pdf" aria-label=" ">paper</a>
 
